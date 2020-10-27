2b6228ff809164b8f6d41b3b5c3eb3d441de8fe1
Gradt <linn.doodles@gmail.com>
2020-10-27 16:44:27 +0300

    Added background + mouseover effect + Created popups

--- js/main.js
+++ js/main.js
@@ -34,6 +34,25 @@
 });
 */
 
+// active menu on scroll
+window.addEventListener('scroll', () => {
+	let scrollDistance = window.scrollY;
+
+	if (window.innerWidth > 768) {
+		document.querySelectorAll('.block__scroll').forEach((el, i) => {
+			if (el.offsetTop - document.querySelector('.header').clientHeight <= scrollDistance) {
+				document.querySelectorAll('.header__list .header__item').forEach((el) => {
+					if (el.classList.contains('active')) {
+						el.classList.remove('active');
+					}
+				});
+
+				document.querySelectorAll('.header__list .header__item')[i].classList.add('active');
+			}
+		});
+	}
+});
+
 // smooth scroll to anchors
 (function () {
     const smoothScroll = function (targetEl, duration) {
@@ -72,7 +91,7 @@
     scrollTo();
 }());
 
-// change philosophy__tabs
+// change tabs
 let tab = function(tabNavigation, tabContent) {
     let tabNav = document.querySelectorAll(tabNavigation);
     let tabDetails = document.querySelectorAll(tabContent);
@@ -105,12 +124,12 @@ tab('.philosophy__tabs .block__item', '.philosophy__details');
 tab('.facilities__tabs .block__item', '.facilities__details');
 
 // change costs__tabs
-tab('.costs__tabs .block__item', '.costs__card');
+tab('.costs__tabs .block__item', '.costs__cards');
 
 // change invitation__tabs
 tab('.invitation__tabs .block__item', '.invitation__form');
 
-// open popups
+// open popup__facilities
 const popups = document.querySelectorAll('.popup');
 const images = document.querySelectorAll('.facilities__image');
 const close = document.querySelectorAll('.popup__close');
@@ -123,3 +142,63 @@ for (let i = 0; i < images.length; i += 1) {
         });
     });
 };
+
+// open popup__request
+const buttonLegal = document.querySelectorAll('.button__legal');
+const buttonPrivate = document.querySelectorAll('.button__private');
+const popupLegal = document.querySelector('.popup__legal');
+const popupPrivate = document.querySelector('.popup__private');
+const popupExit = document.querySelectorAll('.popup__request');
+
+function popupShow(button, popup) {
+    button.forEach(item => {
+        item.addEventListener('click', function() {
+            popup.classList.remove('hidden');
+            popupExit.forEach(exit => {
+                exit.addEventListener('click', function() {
+                    popup.classList.add('hidden');
+                })
+            })
+        });
+    });
+};
+
+popupShow(buttonLegal, popupLegal);
+popupShow(buttonPrivate, popupPrivate);
+
+// slider
+const slider = document.querySelector('.facilities__slider');
+const slides = document.querySelectorAll('.facilities__slides .facilities__image');
+const arrowPrevious = document.querySelector('.arrow__left');
+const arrowNext = document.querySelector('.arrow__right');
+let count = 0;
+
+for (let i = 0; i < slides.length; i += 1) {
+    function next() {
+        arrowNext.addEventListener('click', () => {
+            slides[count].classList.remove('active');
+        })
+    }
+}
+
+// show usability__block buttons on hover
+
+(function() {
+    const cards = document.querySelectorAll('.usability__item');
+
+    cards.forEach(item => {
+        if (!item.classList.contains('active')) {
+            cards[2].classList.add('active');
+        }
+        item.addEventListener('mouseover', () => {
+            if (cards[2].classList.contains('active')) {
+                cards[2].classList.remove('active');
+            }
+            item.classList.add('active');
+        })
+        item.addEventListener('mouseout', () => {
+            item.classList.remove('active');
+            cards[2].classList.add('active');
+        })
+    })
+}());
